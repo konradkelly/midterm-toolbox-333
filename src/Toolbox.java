e3a0713de1fcc6a00c12ae4a922425825cbb6d65
konradkelly <konradky@gmail.com>
2026-02-08 22:50:27 -0800

    implement findHead(DoubleNode tail) method

--- src/Toolbox.java
+++ src/Toolbox.java
@@ -56,7 +56,11 @@ public class Toolbox {
     if (tail == null) {
       throw new IllegalArgumentException("Tail cannot be null.");
     }
-    return null; 
+    DoubleNode current = tail;
+    while (current.prev != null) {
+      current = current.prev;
+    }
+    return current; 
   }
 
   /**
