e176e242ce2b89058bbda64e37af5f929409995c
konradkelly <konradky@gmail.com>
2026-02-08 23:43:47 -0800

    implement insertNode(SingleNode node, SingleNode newNode) method

--- src/Toolbox.java
+++ src/Toolbox.java
@@ -143,7 +143,8 @@ public class Toolbox {
     if (node == null || newNode == null) {
       throw new IllegalArgumentException("Node and newNode cannot be null.");
     }
-
+      newNode.next = node.next;
+      node.next = newNode;   
   }
 
   /**
