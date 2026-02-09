a895d232c2a4ee989b0d85da884b1e5c813b3b28
konradkelly <konradky@gmail.com>
2026-02-08 22:47:06 -0800

    implement findTail(SingleNode head) method

--- src/Toolbox.java
+++ src/Toolbox.java
@@ -38,7 +38,11 @@ public class Toolbox {
     if (head == null) {
       throw new IllegalArgumentException("Head cannot be null.");
     }
-    return null; 
+    SingleNode current = head;
+    while (current.next != null) {
+      current = current.next;
+    }
+    return current; 
   }
 
   /**
