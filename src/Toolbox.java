b132f22182393de339fbde0d48de58123147018c
konradkelly <konradky@gmail.com>
2026-02-08 23:41:24 -0800

    implement findNthElement(SingleNode head, int n) method

--- src/Toolbox.java
+++ src/Toolbox.java
@@ -120,6 +120,15 @@ public class Toolbox {
     if (head == null || n < 0) {
       throw new IllegalArgumentException("Head cannot be null and n cannot be negative.");
     }
+    int idx = 0;
+    SingleNode current = head;
+    while (current != null) {
+      if (idx == n) {
+        return current;
+      }
+      current = current.next;
+      idx++;
+    }
     return null; 
   }
 
